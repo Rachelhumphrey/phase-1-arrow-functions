1a5a3cfbca9ce3279c9d019ddc9f492e60e8fcf7
RachelHumphrey <rachelhumphrey87@gmail.com>
2023-04-13 16:59:14 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function() {
+    return quotient 
+}
+const quotient = 2000 / 100
+
+const square = x => x * 2; 
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+
